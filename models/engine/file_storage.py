96373a2f822a4887163afc37a01b28e3cfa25527
salehgomaa60 <salehgomaazzb@gmail.com>
2024-06-21 14:57:08 -0700

    fixing reload function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -26,70 +26,27 @@ class FileStorage:
             d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(d, f)
 
-    def classes(self):
-        """Returns a dictionary of valid classes and their references"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
-        classes = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review}
-        return classes
-
     def reload(self):
-        """Reloads the stored objects"""
-        if not os.path.isfile(FileStorage.__file_path):
-            return
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
-            obj_dict = json.load(f)
-            obj_dict = {k: self.classes()[v["__class__"]](**v)
-                        for k, v in obj_dict.items()}
-            # TODO: should this overwrite or insert?
-            FileStorage.__objects = obj_dict
-
-    def attributes(self):
-        """Returns the valid attributes and their types for classname"""
-        attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list},
-            "Review":
-            {"place_id": str,
-                         "user_id": str,
-                         "text": str}
-        }
-        return attributes
+        """de-serialize persisted objects"""
+        try:
+            deserialized = {}
+            with open(FileStorage.__file_path, "r") as f:
+                deserialized = json.load(f)
+            for key, obj in deserialized.items():
+                cls_name = obj["__class__"]
+                if cls_name == "BaseModel":
+                    FileStorage.__objects[key] = BaseModel(**obj)
+                elif cls_name == "User":
+                    FileStorage.__objects[key] = User(**obj)
+                elif cls_name == "State":
+                    FileStorage.__objects[key] = State(**obj)
+                elif cls_name == "City":
+                    FileStorage.__objects[key] = City(**obj)
+                elif cls_name == "Amenity":
+                    FileStorage.__objects[key] = Amenity(**obj)
+                elif cls_name == "Place":
+                    FileStorage.__objects[key] = Place(**obj)
+                elif cls_name == "Review":
+                    FileStorage.__objects[key] = Review(**obj)
+        except (FileNotFoundError, JSONDecodeError):
+            pass
