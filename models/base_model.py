0b4ebaa2142096c7c5f9734cab374601badd04ed
salehgomaa60 <salehgomaazzb@gmail.com>
2024-06-27 21:17:34 -0700

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -5,27 +5,48 @@ serve as the base of ou model."""
 
 from uuid import uuid4
 from datetime import datetime
+from models import storage
 
 class BaseModel:
-    """Base class"""
+    """ base class """
 
-    def __init__(self):
-        """Initialize instance attributes."""
-        
-        self.id = str(uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
-        
+    def __init__(self, *args, **kwargs):
+        """ serialize and deserialize class """
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            storage.new(self)
+            return
+
+        """deserialize of kwargs"""
+        if 'id' not in kwargs:
+            kwargs['id'] = str(uuid4())
+        self.id = kwargs['id']
+
+        for key, val in kwargs.items():
+            if key == "__class__":
+                continue
+            setattr(self, key, val)
+        if "created_at" in kwargs:
+            self.created_at = datetime.strptime(
+                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+        if "updated_at" in kwargs:
+            self.updated_at = datetime.strptime(
+                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+
     def __str__(self):
-        """ str representation of object """
-        
+        """overide str representation of self"""
         fmt = "[{}] ({}) {}"
-        return fmt.format(type(self).__name__, self.id, self.__dict__)
-    
+        return fmt.format(
+            type(self).__name__,
+            self.id,
+            self.__dict__)
+
     def save(self):
         """updating instance variable updated at """
-        
         self.updated_at = datetime.utcnow()
+        storage.save()
         
 
     def to_dict(self):
